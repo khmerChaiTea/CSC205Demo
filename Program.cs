e51e1240156e1623f39845b802985b573577a205
pchai <prempreychhay@gmail.com>
2024-07-15 15:41:38 -0700

    3:41

--- Program.cs
+++ Program.cs
@@ -1,43 +1,16 @@
 ﻿using System;
-namespace ThinkSharp
+
+namespace CSC205
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            int[] scores = RandomArray(30);
-            // Print output of n(30) amount of random number from method RandomArray
-            WriteArray(scores);
-            int[] counts = new int[100];
-            for (int i = 0; i < scores.Length; i++)
-            {
-                /* 
-                // look at index if the value equal index count it
-                int index = scores[i];
-                // Imcrement count of value in array
-                counts[index]++;
-                */
-                counts[scores[i]]++;
-            }
-            // Output the count of number in the array
-            WriteArray(counts);
+            Time time = new Time();
+            Console.WriteLine(time.hour);
+            Console.WriteLine(time.minute);
+            Console.WriteLine(time.second);
         }
-        public static int[] RandomArray(int size)
-        {
-            Random random = new Random();
-            int[] array = new int[size];
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = random.Next(0, 100);
-            }
-            return array;
-        }
-        public static void WriteArray(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
-        }
+
     }
 }
---
+++ Time.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205
+{
+    internal class Time
+    {
+        public int hour, minute;
+        public double second;
+    }
+}
