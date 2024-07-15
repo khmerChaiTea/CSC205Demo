59a7cde78383c0dea6790ae0cf8923c7e6d98980
pchai <prempreychhay@gmail.com>
2024-07-15 15:52:14 -0700

    3:52

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ namespace CSC205
     {
         public static void Main(string[] args)
         {
-            Time time = new Time();
-            Console.WriteLine(time.hour);
-            Console.WriteLine(time.minute);
-            Console.WriteLine(time.second);
+            Time t1 = new Time();
+            Console.WriteLine(t1.hour);
+            Console.WriteLine(t1.minute);
+            Console.WriteLine(t1.second);
+
+            Time t2 = new Time();
+            Console.WriteLine(t2.hour);
+            Console.WriteLine(t2.minute);
+            Console.WriteLine(t2.second);
+
+            Time t3 = new Time(3, 50, 23);
+            Console.WriteLine(t3.hour);
+            Console.WriteLine(t3.minute);
+            Console.WriteLine(t3.second);
         }
 
     }
--- Time.cs
+++ Time.cs
@@ -6,9 +6,26 @@ using System.Threading.Tasks;
 
 namespace CSC205
 {
-    internal class Time
+    public class Time
     {
         public int hour, minute;
         public double second;
+
+        // Here is an example for the Time Class:
+        // This code below is the comstructor (default)
+        public Time()
+        {
+            // We drop 'this.'
+            hour = 1;
+            minute = 2;
+            second = 3.0;
+        }
+
+        public Time(int hour, int minute, double second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
     }
 }
