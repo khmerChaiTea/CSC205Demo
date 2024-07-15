d887d7e1f42b85d942617977687a3ba7cf7490b0
pchai <prempreychhay@gmail.com>
2024-07-15 14:29:36 -0700

    2:29

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ public class Program
     public static void Main(String[] args)
     {
         int a2 = 1;
-        Console.WriteLine(a2);  // Output 1
+        Console.WriteLine(a2);  // Output 1 takes a2 = 1 and out put 1
         // no parameter or arguement to pass
-        ModifyNumber();
-        Console.WriteLine(a2);  // Output 1
+        ModifyNumber();         // Call the method and output 2
+        Console.WriteLine(a2);  // Output 1 since no parameter or arguement to pass through
     }
 
     static void ModifyNumber()
     {
         // Take the global
         a2++;
-        Console.WriteLine(a2);  // Output 2
+        Console.WriteLine(a2);  // Increment global by 1
     }
 }
 
