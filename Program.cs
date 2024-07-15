ead0d5a37ed3dacb791d2f5c0b9af72b25e9229c
pchai <prempreychhay@gmail.com>
2024-07-15 14:45:13 -0700

    2:45

--- Program.cs
+++ Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 
-public class Program
+namespace ThinkSharp
 {
-    // Used static variable
-    static int a2 = 1;
-    public static void Main(String[] args)
+    public class Program
     {
-        // Generate and display 10 random 
-        // floating numbers between 0 and 1.0 
-        Random random = new Random();
-        for (int i = 0; i < 10; i++)
+        public static void Main(string[] args)
         {
-            double x = random.NextDouble() * 10;
-            Console.WriteLine(x);
+            // Call methods WriteArray and RandomArray by giving 10 as the parameter/arguement
+            WriteArray(RandomArray(10));
         }
 
+        public static int[] RandomArray(int size)
+        {
+            Random random = new Random();
+            // Set array size to given "size" from the passing arguement as parameter
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)  // array.Length should be same as size
+            {
+                array[i] = random.Next(0, 100);     // generate up to 100 random number
+            }
+            return array;   // return the array
+        }
+
+        public static void WriteArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                // Print the output for each iteration with a given parameter
+                Console.WriteLine(array[i]);
+            }
+        }
     }
 }
 
