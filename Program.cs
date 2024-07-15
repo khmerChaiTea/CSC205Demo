af3dbac861e1c516c02061d57774ac3375e840a3
pchai <prempreychhay@gmail.com>
2024-07-15 13:46:39 -0700

    1:46

--- Program.cs
+++ Program.cs
@@ -4,20 +4,21 @@ public class Program
 {
     public static void Main(String[] args)
     {
-        //int[] a = new int[4];
-        //int[] a = new int[4] { 1, 2, 3, 4 };
         int[] a = { 1, 2, 3, 4 };
-
-        foreach (int i in a)
+        //int[] b = a;
+        int[] b = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
         {
-            Console.WriteLine(i);
+            b[i] = a[i];
         }
 
-        int[] b = a;
-        foreach (int i in b)
-        {
-            Console.WriteLine(i);
-        }
+        b[0] = 5;
+
+        Console.WriteLine("array a:");
+        foreach (int i in a) Console.WriteLine(i);
+
+        Console.WriteLine("array b:");
+        foreach (int i in b) Console.WriteLine(i);
     }
 
 }
