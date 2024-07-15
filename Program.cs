c725061e6d94bf40669adbc871c070997bd7e657
pchai <prempreychhay@gmail.com>
2024-07-15 14:56:00 -0700

    2:55

--- Program.cs
+++ Program.cs
@@ -1,32 +1,48 @@
 ﻿using System;
-
 namespace ThinkSharp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            // Call methods WriteArray and RandomArray by giving 10 as the parameter/arguement
-            WriteArray(RandomArray(10));
+            int[] scores = RandomArray(20);
+            WriteArray(scores);
+            int a = InRange(scores, 90, 100);
+            int b = InRange(scores, 80, 90);
+            int c = InRange(scores, 70, 80);
+            int d = InRange(scores, 60, 70);
+            int f = InRange(scores, 0, 60);
+            Console.WriteLine($"{a}, {b}, {c}, {d}, {f}");
+        }
+
+        public static int InRange(int[] array, int low, int high)
+        {
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item >= low && item < high)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
 
         public static int[] RandomArray(int size)
         {
             Random random = new Random();
-            // Set array size to given "size" from the passing arguement as parameter
             int[] array = new int[size];
-            for (int i = 0; i < array.Length; i++)  // array.Length should be same as size
+            for (int i = 0; i < array.Length; i++)
             {
-                array[i] = random.Next(0, 100);     // generate up to 100 random number
+                array[i] = random.Next(0, 100);
             }
-            return array;   // return the array
+            return array;
         }
 
         public static void WriteArray(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
-                // Print the output for each iteration with a given parameter
                 Console.WriteLine(array[i]);
             }
         }
