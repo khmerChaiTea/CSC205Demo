5f0fb6463b38f9969e5958204c783088385d7383
pchai <prempreychhay@gmail.com>
2024-07-17 13:50:00 -0700

    1:49

--- Program.cs
+++ Program.cs
@@ -15,10 +15,10 @@ namespace CSC205
             Time t3 = new Time(3, 50, 23);
             Console.WriteLine(t3);
             t3.WriteTime();
-            t3.WriteTime2(t3);
+            t3.WriteTime2(t3);  // Instance method
             Time.WriteTime(t3);
             Console.WriteLine("--------------------");
-            t1.WriteTime2(t3);
+            t1.WriteTime2(t3);  // Best if passing a time to another
         }
     }
 }
