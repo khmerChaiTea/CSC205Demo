e37a695bbe6df2eb7a96f970f3ad59fa417f444c
pchai <prempreychhay@gmail.com>
2024-07-17 15:26:43 -0700

    v4 3:26

--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 
-// version 3
+// version 4
 public class Program
 {
     static void Main(string[] args)
     {
-        //Person person = new Person("Allan", 21);
         Teacher teacher = new Teacher("Tom", 55, "Computer Science");
-        teacher.DisplayNameAge();
+        teacher.DisplayTeacher();
         Student student = new Student("Sara", 19, 3.5);
-        student.DisplayNameAge();
+        student.DisplayStudent();
     }
 }
 
-abstract class Person // Parent class cannot be instantiated
-{
+abstract class Person
+{ // Parent class
     public string name;
     public int age;
     public Person(string name, int age)
@@ -23,17 +22,27 @@ abstract class Person // Parent class cannot be instantiated
         this.age = age;
     }
     public void DisplayNameAge()
-    { System.Console.WriteLine($"{name} is {age} years old."); }
+    { System.Console.WriteLine($"Name：{name}, Age: {age}"); }
 }
 class Teacher : Person
-{ // Child class 
+{ // Child class
     public string subject;
     public Teacher(string name, int age, string subject) : base(name, age)
     { this.subject = subject; }
+    public void DisplayTeacher()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"Teaches {subject}\n");
+    }
 }
 class Student : Person
 { // Child class
     public double gpa;
     public Student(string name, int age, double gpa) : base(name, age)
     { this.gpa = gpa; }
+    public void DisplayStudent()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"GPA: {this.gpa}");
+    }
 }
