4ab1c37d7ad185f4399c0e47bb5e0609d43d862b
pchai <prempreychhay@gmail.com>
2024-07-17 14:25:17 -0700

    2:25

--- Program.cs
+++ Program.cs
@@ -16,17 +16,23 @@ public class Book
         isbn = i;
         price = p;
     }
+    public void Display()
+    { Console.WriteLine($"Title: {title}, ISBN: {isbn}, Price: {price}"); }
 }
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        Book book1 = new Book("C# 9 and .NET 5", "180056810X", 42.74);
-        Console.WriteLine(book1.title);
-        Book book2 = new Book("C# in Depth", "1617294535", 38.99);
-        Console.WriteLine(book2.title);
-        Book book3 = new Book();
-        Console.WriteLine("book 3 title: " + book3.title);
+        Book[] books = new Book[5];
+        books[0] = new Book("book 1", "121", 10.99);
+        books[1] = new Book("book 2", "122", 12.99);
+        books[2] = new Book("book 3", "123", 18.88);
+        books[3] = new Book("book 4", "124", 5.99);
+        books[4] = new Book("book 5", "125", 20.0);
+
+        books[4].Display();
     }
 }
+
 
