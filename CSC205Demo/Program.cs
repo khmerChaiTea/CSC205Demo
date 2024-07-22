42d9fa2ee8cbe3ab5d85446662bc353473c9bded
pchai <prempreychhay@gmail.com>
2024-07-22 15:08:11 -0700

    nothing out put

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 public class NonStaticClass
 {
-    public string staticStr = "A non-static variable (or data member)";
+    // public string staticStr = "A non-static variable (or data member)";
+    public string Str {  get; set; }
     public void NonStaticMethod()
     {
-        Console.WriteLine(staticStr);
+        Console.WriteLine(Str);
     }
 }
 public class Program
@@ -12,11 +13,7 @@ public class Program
     static void Main(string[] args)
     {
         NonStaticClass sc = new NonStaticClass(); // can be instantiated!
-        Console.WriteLine(sc.staticStr);
-        sc.NonStaticMethod();
-
-        NonStaticClass sc2 = new NonStaticClass(); // can be instantiated!
-        Console.WriteLine(sc.staticStr);
+        Console.WriteLine(sc.Str);
         sc.NonStaticMethod();
     }
 }
