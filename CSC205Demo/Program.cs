75721f5472ca3b264f90bbf0b95e3fd3d73961b8
pchai <prempreychhay@gmail.com>
2024-07-22 13:30:41 -0700

    OOP WK 4 note

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,95 +1,50 @@
 ﻿using System;
 namespace ThinkSharp
 {
-    public class Time
-    {
-        public int Hour, Minute;
-        public double Second;
-        public Time()
-        { this.Hour = 0; this.Minute = 0; this.Second = 0.0; }
-        public Time(int hour, int minute, double second)
-        {
-            this.Hour = hour; this.Minute = minute;
-            this.Second = second;
-        }
-        public Time(double seconds)
-        {
-            Hour = (int)(seconds / 3600.0);
-            seconds -= Hour * 3600.0;
-            Minute = (int)(seconds / 60.0);
-            Second = seconds - (Minute * 60.0);
-        }
-        public double ToSeconds()
-        {
-            int minutes = Hour * 60 + Minute;
-            return minutes * 60 + Second;
-        }
-        public string ToString()
-        {
-            return String.Format("{0:D2}:{1:D2}:{2:D2}",
-            Hour, Minute, (int)Second);
-        }
-        public bool IsAfter(Time time)
-        { return ToSeconds() > time.ToSeconds(); }
-        public void AddTime(Time time)
-        {
-            Hour += time.Hour;
-            Minute += time.Minute;
-            Second += time.Second;
-            adjustForOverflow();
-        }
-        public void Increment(double seconds)
-        { Second += seconds; adjustForOverflow(); }
-        private void adjustForOverflow()
-        {
-            while (Second >= 60.0)
-            { Second -= 60.0; Minute += 1; }
-            while (Minute > 59)
-            { Minute -= 60; Hour += 1; }
-        }
-    } // end of class Time
+    // version 4
     public class Program
     {
         static void Main(string[] args)
         {
-            Time t = new Time();
-            Console.WriteLine(t.ToString());
-            var dt1 = new DateAndTime();
-            var dt2 = new DateAndTime(20, 4, 2014, 12, 0, 0);
-            Console.WriteLine(dt1.ToString());
-            Console.WriteLine($"{dt2.Year}, {dt2.Month},  {dt2.Day}, {dt2.Hour}, {dt2.Minute}, {dt2.Second}");
+            Teacher teacher = new Teacher("Tom", 55, "Computer Science");
+            teacher.DisplayTeacher();
+            Student student = new Student("Sara", 19, 3.5);
+            student.DisplayStudent();
         }
     }
 
-    public class DateAndTime : Time
-    {
-        public int Day, Month, Year;
-        public DateAndTime() : base()
-        { Day = 0; Month = 0; Year = 0; }
-        public DateAndTime(int day, int month, int year,
-        int hour, int minute, double second)
-        : base(hour, minute, second)
+    abstract class Person
+    { // Parent class
+        public string name;
+        public int age;
+        public Person(string name, int age)
         {
-            Day = day;
-            Month = month;
-            Year = year;
+            this.name = name;
+            this.age = age;
         }
-
-        public void Increment(double seconds)
+        public void DisplayNameAge()
+        { System.Console.WriteLine($"Name：{name}, Age: {age}"); }
+    }
+    class Teacher : Person
+    { // Child class
+        public string subject;
+        public Teacher(string name, int age, string subject) : base(name, age)
+        { this.subject = subject; }
+        public void DisplayTeacher()
         {
-            base.Increment(seconds);
-            adjustForOverflow();
+            base.DisplayNameAge();  // Can keep base or drop it
+            System.Console.WriteLine($"Teaches {subject}\n");
         }
-
-        private void adjustForOverflow()
+    }
+    class Student : Person
+    { // Child class
+        public double gpa;
+        public Student(string name, int age, double gpa) : base(name, age)
+        { this.gpa = gpa; }
+        public void DisplayStudent()
         {
-            while (Hour > 23)
-            {
-                Hour -= 24;
-                Day += 1;
-            }
-            // TODO: Day overflows into Month
-            // TODO: Month overflows into Year
+            base.DisplayNameAge();
+            System.Console.WriteLine($"GPA: {this.gpa}");
         }
     }
 }
