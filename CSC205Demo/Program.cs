6ddea71a68f2983b75b5a6e802be964ff7591290
pchai <prempreychhay@gmail.com>
2024-07-22 15:30:28 -0700

    updated 3:30

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -2,7 +2,7 @@
 
 public class Counter
 {
-    public static int instances = 0;
+    public int instances = 0;
     public Counter()
     { instances++; }
 }
@@ -11,15 +11,15 @@ public class App
     static void Main(String[] args)
     {
         Counter c0 = new Counter();
-        Console.WriteLine($"{Counter.instances} instance(s) of the Counter class!");
+        Console.WriteLine($"{c0.instances} instance(s) of the Counter class!");
         Counter c1 = new Counter();
-        Console.WriteLine($"{Counter.instances} instance(s) of the Counter class!");
+        Console.WriteLine($"{c1.instances} instance(s) of the Counter class!");
         Counter c2 = new Counter();
-        Console.WriteLine($"{Counter.instances} instance(s) of the Counter class!");
+        Console.WriteLine($"{c2.instances} instance(s) of the Counter class!");
         Counter c3 = new Counter();
-        Console.WriteLine($"{Counter.instances} instance(s) of the Counter class!");
+        Console.WriteLine($"{c3.instances} instance(s) of the Counter class!");
         Counter c4 = new Counter();
-        Console.WriteLine($"{Counter.instances} instance(s) of the Counter class!");
+        Console.WriteLine($"{c4.instances} instance(s) of the Counter class!");
     }
 }
 
