fe72178f384d750c1447b3d72738938165de21fc
pchai <prempreychhay@gmail.com>
2024-07-24 14:32:04 -0700

    override 7/24 2:32

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 
-public class Counter
+abstract class Shape
 {
-    public int instances = 0;
-    public Counter()
-    { instances++; }
-}
-public class App
-{
-    static void Main(String[] args)
+    public virtual int GetArea()
     {
-        Counter c0 = new Counter();
-        Console.WriteLine($"{c0.instances} instance(s) of the Counter class!");
-        Counter c1 = new Counter();
-        Console.WriteLine($"{c1.instances} instance(s) of the Counter class!");
-        Counter c2 = new Counter();
-        Console.WriteLine($"{c2.instances} instance(s) of the Counter class!");
-        Counter c3 = new Counter();
-        Console.WriteLine($"{c3.instances} instance(s) of the Counter class!");
-        Counter c4 = new Counter();
-        Console.WriteLine($"{c4.instances} instance(s) of the Counter class!");
-        Console.WriteLine($"{c0.instances} instance(s) of the Counter class!");
+        return 1;
     }
 }
 
+class Square : Shape
+{
+    private int _side;
+
+    public Square(int n) => _side = n;
+
+    // GetArea method is required to avoid a compile-time error.
+    public override int GetArea() => _side * _side;
+
+    static void Main()
+    {
+        var sq = new Square(12);
+        Console.WriteLine($"Area of the square = {sq.GetArea()}");
+    }
+} // Output: Area of the square = 144
