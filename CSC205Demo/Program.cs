5ecf8ee47380f7b22548237b09293994774e64b9
pchai <prempreychhay@gmail.com>
2024-07-29 15:44:21 -0700

    Making a rectangle class

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -13,11 +13,32 @@ class Square : Shape
 
     // GetArea method is required to avoid a compile-time error.
     public override int GetArea() => _side * _side;
+}
+// Output: Area of the square = 144z
 
+class Rectangle : Shape
+{
+    private int _length;
+    private int _width;
+
+    public Rectangle(int l, int w)
+    {
+        _length = l;
+        _width = w;
+    }
+
+    // GetArea method is required to avoid a compile-time error.
+    public override int GetArea() => _length * _width;
+}
+
+class App
+{
     static void Main()
     {
         var sq = new Square(12);
         Console.WriteLine($"Area of the square = {sq.GetArea()}");
+
+        var rect = new Rectangle(5, 7);
+        Console.WriteLine($"Area of the rectangle = {rect.GetArea()}");
     }
 }
-// Output: Area of the square = 144z
