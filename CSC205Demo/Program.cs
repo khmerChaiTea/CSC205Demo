75b1e06a682607ccd0ffb64ae7828c40c744266c
pchai <prempreychhay@gmail.com>
2024-07-29 15:29:24 -0700

    Example of Abstract class

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,27 +1,23 @@
 ﻿using System;
-public class Book // change class to struct to see the difference
+
+abstract class Shape
 {
-    public string title, isbn;
-    public double price;
+    public abstract int GetArea();
 }
-public class Program
+
+class Square : Shape
 {
-    public static void Main(string[] args)
+    private int _side;
+
+    public Square(int n) => _side = n;
+
+    // GetArea method is required to avoid a compile-time error.
+    public override int GetArea() => _side * _side;
+
+    static void Main()
     {
-        Book book1 = new Book();
-        book1.title = "Think Sharp";
-        book1.isbn = "N/A";
-        book1.price = 0.0;
-        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
-
-        Book book2 = book1;
-        Console.WriteLine($"title: {book2.title}, ISBN: {book2.isbn}, Price: {book2.price}");
-
-        book2.title = "The C# Player's Guide";
-        book2.isbn = "978-0985580155";
-        book2.price = 34.95;
-
-        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
-        Console.WriteLine($"title: {book2.title}, ISBN: {book2.isbn}, Price: {book2.price}");
+        var sq = new Square(12);
+        Console.WriteLine($"Area of the square = {sq.GetArea()}");
     }
 }
+// Output: Area of the square = 144z
