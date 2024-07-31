60a02090c21792e09766969c940ef811e388f7f6
pchai <prempreychhay@gmail.com>
2024-07-31 13:31:22 -0700

    Updated 7/31 13:31

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -36,11 +36,22 @@ class App
 {
     static void Main()
     {
-        Shape[] shapes = { new Square(5), new Rectangle(3, 4) };
+        //Shape[] shapes = { new Square(5), new Rectangle(3, 4) };
+        Object[] shapes = { new Square(5), new Rectangle(3, 4) };
         //Shape sq = new Square(12);
-        Console.WriteLine($"Area of the square = {shapes[0].GetArea()}");
+        //Console.WriteLine($"Area of the square = {shapes[0].GetArea()}");
+        Console.WriteLine($"Area of the square = {((Shape)shapes[0]).GetArea()}");
+        /*
+         * shapes[0]: This accesses the first element in the array, which is an instance of Square.
+         * (Shape)shapes[0]: This casts the object at shapes[0] to the Shape type.
+         * Since Square is a subclass of Shape, this cast is valid.
+         * ((Shape)shapes[0]).GetArea(): This calls the GetArea method on the cast object.
+         * If GetArea is a virtual or abstract method defined in the Shape class
+         * and overridden in the Square class, the correct method implementation
+         * will be invoked for the Square instance.
+        */
 
         //Shape rect = new Rectangle(5, 7);
-        Console.WriteLine($"Area of the rectangle = {shapes[1].GetArea()}");
+        //Console.WriteLine($"Area of the rectangle = {shapes[1].GetArea()}");
     }
 }
