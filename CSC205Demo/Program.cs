5b8741524400ca64c3baf579276b2ee5bc34a9f8
pchai <prempreychhay@gmail.com>
2024-07-31 14:26:08 -0700

    Updated the Cylinder method 14:26

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -11,41 +11,34 @@ class TestClass
     public class Circle : Shape
     {
         public double R {  get; set; }
-        //private double r;
-        //public Circle(double r)
-        //{
-        //    this.r = r;
-        //}
 
-        public override double Area() => Math.PI * R * R;
-
-        //public override double Area()
-        //{
-        //    return PI * r * r;
-        //}
-        // A = PI * r^2
+        public override double Area() => PI * R * R;
     }
     public class Cylinder : Shape
     {
-        private double r, h;
+        public double R { get; set; }
+        public double H { get; set; }
+        //private double r, h;
 
-        public Cylinder(double r, double h)
-        {
-            this.r = r;
-            this.h = h;
-        }
-        public override double Area()
-        {
-            return 2 * PI * r * r + 2 * PI * r * h;
-        }
+        //public Cylinder(double r, double h)
+        //{
+        //    this.r = r;
+        //    this.h = h;
+        //}
+
+        public override double Area() => 2 * PI * R * R + 2 * PI * R * H;
+        //public override double Area()
+        //{
+        //    return 2 * PI * r * r + 2 * PI * r * h;
+        //}
         // V = PI * r^2 * h; V = Volume
         // Total Surface Area; A(total) = A(lateral) + A(bases) = 2 * PI * r^2 + 2 * PI * r * h
     }
     static void Main()
     {
         double r = 3.0, h = 5.0;
-        Shape c = new Circle() {R=r};   // Property initializer
-        Shape l = new Cylinder(r, h);
+        Shape c = new Circle() {R = r};   // Property initializer
+        Shape l = new Cylinder() {R = r, H = h};
         // Display results.
         Console.WriteLine("Area of Circle   = {0:F2}", c.Area());
         Console.WriteLine("Area of Cylinder = {0:F2}", l.Area());
