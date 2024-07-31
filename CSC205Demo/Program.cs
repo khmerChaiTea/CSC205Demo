1ad139173f0c32e5b1cc8c48c8bd171151b424fe
pchai <prempreychhay@gmail.com>
2024-07-31 15:29:54 -0700

    No constructor

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -4,21 +4,21 @@ public class Cat
     // Auto-implemented properties.
     public int Age { get; set; }
     public string Name { get; set; }
-    public Cat() { }
-    public Cat(string name)
-    {
-        this.Name = name;
-    }
+    //public Cat() { }
+    //public Cat(string name)
+    //{
+    //    this.Name = name;
+    //}
 }
 class Program
 {
     static void Main(string[] args)
     {
         Cat cat = new Cat { Age = 10, Name = "Fluffy" };
-        Cat sameCat = new Cat("Fluffy") { Age = 10 };
+        //Cat sameCat = new Cat("Fluffy") { Age = 10 };
         Cat sameSameCat = new Cat() { Age = 10, Name = "Fluffy" };
         Console.WriteLine($"cat name: {cat.Name}, cat age: {cat.Age}");
-        Console.WriteLine($"cat name: {sameCat.Name}, cat age: {sameCat.Age}");
+        //Console.WriteLine($"cat name: {sameCat.Name}, cat age: {sameCat.Age}");
         Console.WriteLine($"cat name: {sameSameCat.Name}, cat age: {sameSameCat.Age}");
     }
 }
