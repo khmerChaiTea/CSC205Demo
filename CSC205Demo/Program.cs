ed306664fdf71b70fa2f4ac73ace0c89c6d4091a
pchai <prempreychhay@gmail.com>
2024-07-31 14:32:15 -0700

    updated make the code cleaner

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,39 +1,29 @@
 ﻿using System;
 
-class TestClass
-{
-    public abstract class Shape
+public abstract class Shape
     {
         public const double PI = Math.PI;
 
         public abstract double Area();  // class must be abstract
     }
-    public class Circle : Shape
+public class Circle : Shape
     {
         public double R {  get; set; }
 
         public override double Area() => PI * R * R;
     }
-    public class Cylinder : Shape
+public class Cylinder : Shape
     {
         public double R { get; set; }
         public double H { get; set; }
-        //private double r, h;
-
-        //public Cylinder(double r, double h)
-        //{
-        //    this.r = r;
-        //    this.h = h;
-        //}
 
         public override double Area() => 2 * PI * R * R + 2 * PI * R * H;
-        //public override double Area()
-        //{
-        //    return 2 * PI * r * r + 2 * PI * r * h;
-        //}
         // V = PI * r^2 * h; V = Volume
         // Total Surface Area; A(total) = A(lateral) + A(bases) = 2 * PI * r^2 + 2 * PI * r * h
     }
+
+class TestClass
+{
     static void Main()
     {
         double r = 3.0, h = 5.0;
