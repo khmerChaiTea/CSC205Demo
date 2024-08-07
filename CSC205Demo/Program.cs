e8c45aeb3a09118ae7993c7cf34463c86e5b29d6
pchai <prempreychhay@gmail.com>
2024-08-07 14:16:59 -0700

    Generic Linked List Example

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -1,21 +1,40 @@
-﻿// https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/arrays-and-collections?WT.mc_id=Educationalcsharp-c9-scottha
-using System;
+﻿using System;
 using System.Collections.Generic;
-public class Program
+public class Example
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        var fibonacciNumbers = new List<int> { 1, 1 };
-        while (fibonacciNumbers.Count < 10)
-        {
-            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-            fibonacciNumbers.Add(previous + previous2);
-        }
-
-        foreach (var number in fibonacciNumbers)
-        {
-            Console.WriteLine(number);
-        }
+        // Create the link list.
+        string[] words =
+            { "the", "fox", "jumps", "over", "the", "dog" };
+        LinkedList<string> sentence = new LinkedList<string>(words);
+        Display(sentence, "The linked list values:");
+        Console.WriteLine("sentence.Contains(\"jumps\") = {0}", sentence.Contains("jumps"));
+        // Add the word 'today' to the beginning of the linked list.
+        sentence.AddFirst("today");
+        Display(sentence, "Test 1: Add 'today' to beginning of the list:");
+        // Move the first node to be the last node.
+        LinkedListNode<string> mark1 = sentence.First;
+        sentence.RemoveFirst();
+        sentence.AddLast(mark1);
+        Display(sentence, "Test 2: Move first node to be last node:");
+        // Change the last node to 'yesterday'.
+        sentence.RemoveLast();
+        sentence.AddLast("yesterday");
+        Display(sentence, "Test 3: Change the last node to 'yesterday':");
+        // Move the last node to be the first node.
+        mark1 = sentence.Last;
+        sentence.RemoveLast();
+        sentence.AddFirst(mark1);
+        Display(sentence, "Test 4: Move last node to be first node:");
     }
-}
+    private static void Display(LinkedList<string> words, string test)
+    {
+        Console.WriteLine(test);
+        foreach (string word in words)
+        {
+            Console.Write(word + " ");
+        }
+        Console.Write("\n\n");
+    }
+} // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=net-8.0
