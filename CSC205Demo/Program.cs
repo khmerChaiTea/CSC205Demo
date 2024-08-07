030146cf2aca39c37c7a227981bfb540fa630cfb
pchai <prempreychhay@gmail.com>
2024-08-07 13:46:23 -0700

    Capacity is 4 by default

--- CSC205Demo/Program.cs
+++ CSC205Demo/Program.cs
@@ -6,9 +6,13 @@ public class SamplesArrayList
     {
         // Creates and initializes a new ArrayList.
         ArrayList myAL = new ArrayList();
-        myAL.Add("Hello");
-        myAL.Add("World");
-        myAL.Add("!");
+        for (int i = 0; i < 2; i++)
+        {
+            myAL.Add("Hello");
+        }
+
+        //myAL.Add("World");
+        //myAL.Add("!");
         // Displays the properties and values of the ArrayList.
         Console.WriteLine("myAL");
         Console.WriteLine("    Count:    {0}", myAL.Count);
